3a4533584d60766c86fa20270192d4f7073a1c7b
Harish D <dharish8@gmail.com>
2025-04-04 16:15:37 +0530

    intial version of gateway global

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection></HeroSection>
+      <FooterSection />  
+    </div>
+  );
+}
