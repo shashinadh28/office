ce7074b54339f77c058b306e248aee677ba8862d
Harish D <dharish8@gmail.com>
2025-04-11 17:39:52 +0530

    intial build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["res.cloudinary.com"],
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = {
+  distDir: 'build',
+}
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
-import Image from "next/image";
 
 export default function Home() {
   return (
