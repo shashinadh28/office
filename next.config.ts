4b9d62c1f214959eedc2d06f7fb8db92fefcadc1
Harish D <dharish8@gmail.com>
2025-05-24 23:28:34 +0530

    fix local build issue

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
   distDir: "build",
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
