7d11fd9c107779426a15dc97b6e8020ac6a1e1d3
Harish D <dharish8@gmail.com>
2025-06-26 18:29:00 +0530

    fix: updated text

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ContactButton = () => {
   return (
     <button className="cursor-pointer relative bg-white/20 py-2 rounded-full min-w-[8.5rem] min-h-[2.92rem] group max-w-full flex items-center justify-start hover:bg-teal-500 transition-all duration-[0.8s] ease-[cubic-bezier(0.510,0.026,0.368,1.016)] shadow-[inset_1px_2px_5px_#00000080]">
@@ -7,17 +5,26 @@ const ContactButton = () => {
         <div className="w-[0%] group-hover:w-full transition-all duration-[1s] ease-[cubic-bezier(0.510,0.026,0.368,1.016)]" />
         <div className="rounded-full shrink-0 flex justify-center items-center shadow-[inset_1px_-1px_3px_0_black] h-full aspect-square bg-teal-500 transition-all duration-[1s] ease-[cubic-bezier(0.510,0.026,0.368,1.016)] group-hover:bg-teal-700">
           <div className="size-[0.8rem] text-white group-hover:text-white group-hover:-rotate-45 transition-all duration-[1s] ease-[cubic-bezier(0.510,0.026,0.368,1.016)]">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" height="100%" width="100%">
-              <path fill="currentColor" d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9Z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 16"
+              height="100%"
+              width="100%"
+            >
+              <path
+                fill="currentColor"
+                d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9Z"
+              />
             </svg>
           </div>
         </div>
       </div>
       <div className="pl-[3.4rem] pr-[1.1rem] group-hover:pl-[1.1rem] group-hover:pr-[3.4rem] transition-all duration-[1s] ease-[cubic-bezier(0.510,0.026,0.368,1.016)] text-gray-800 group-hover:text-white">
-        Contacts
+        Contact us
       </div>
     </button>
   );
-}
+};
 
-export default ContactButton; 
+export default ContactButton;
