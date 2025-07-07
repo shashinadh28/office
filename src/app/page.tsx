a41f73cdffdd5f0449da0bab4355f096a6f17446
Harish D <dharish8@gmail.com>
2025-07-07 22:29:23 +0530

    updated footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,14 @@ import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import Hero2 from "@/components/hero2";
 import Hero3 from "@/components/hero3";
-import Hero4 from "@/components/hero4";
-import dynamic from 'next/dynamic';
-
-const Hero5 = dynamic(() => import('@/components/hero5'), {
-  loading: () => <div className="h-40 bg-gray-100 animate-pulse rounded"></div>,
-  ssr: false
-});
-import Hero6 from "@/components/hero6";
-import ClientStories from "@/components/ClientStories";
-import ScrollAnimationWrapper from "@/components/ScrollAnimationWrapper";
 import HiringProcess from "@/components/HiringProcess";
+import ScrollAnimationWrapper from "@/components/ScrollAnimationWrapper";
+import dynamic from "next/dynamic";
+
+const Hero5 = dynamic(() => import("@/components/hero5"), {
+  loading: () => <div className="h-40 bg-gray-100 animate-pulse rounded"></div>,
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -21,29 +18,21 @@ export default function Home() {
       <ScrollAnimationWrapper animation="fadeIn" duration={0.8}>
         <HeroSection />
       </ScrollAnimationWrapper>
-
       <ScrollAnimationWrapper animation="slideUp" duration={0.8}>
         <Hero2 />
       </ScrollAnimationWrapper>
-
       <Hero3 />
-
       <ScrollAnimationWrapper animation="slideUp" duration={0.8}>
-        <HiringProcess /> 
+        <HiringProcess />
       </ScrollAnimationWrapper>
-
       <ScrollAnimationWrapper animation="fadeIn" duration={0.8}>
         <Hero5 />
       </ScrollAnimationWrapper>
-
       {/* TODO: Temporarily commented out ClientStories while gathering information */}
       {/* <ScrollAnimationWrapper animation="slideUp" duration={0.8}>
         <ClientStories />
       </ScrollAnimationWrapper> */}
-
-      <ScrollAnimationWrapper animation="fadeIn" duration={0.8}>
-        <FooterSection />
-      </ScrollAnimationWrapper>
+      <FooterSection />
     </div>
   );
 }
